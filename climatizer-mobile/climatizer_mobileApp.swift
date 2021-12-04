05f5829b54bfffbca27f9f098d6ae3ff07ed948f
Yevhenii <yevhenii.bakhmat@nure.ua>
2021-12-04 15:55:09 +0200

    Initial Commit

---
+++ climatizer-mobile/climatizer_mobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  climatizer_mobileApp.swift
+//  climatizer-mobile
+//
+//  Created by Евгений Бахмат on 04.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct climatizer_mobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
